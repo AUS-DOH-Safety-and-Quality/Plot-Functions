35f15ca16fb2cc8864f82182d85d9894b7fb07a7
Clarke <he198926@health.wa.gov.au>
2022-06-23 12:52:32 +0800

    added total column

--- Num_den_table.R
+++ Num_den_table.R
@@ -2,14 +2,14 @@
 num_den_table <- function(input_df){
 
   num_den_df <- input_df %>%
-    mutate(value = format(round((numerator/denominator)*multiplier, 2),nsmall = 2))
+    mutate(value = format(round((numerator/denominator)*multiplier, digits = 1),nsmall = 2))
   num_den_df <- arrange(num_den_df, period_end)
   num_den_df$value <- if_else(num_den_df$value == " NaN", "0.00", num_den_df$value)
-  num_den_df$value <- if_else(num_den_df$value == "NaN", "Test", num_den_df$value)
   #Rounding up for probabilities for outcomes
-  num_den_df$denominator <- round(num_den_df$denominator, 2)
+  num_den_df$denominator <- round(num_den_df$denominator, 1)
   # Reformat the date column for our preferred style (e.g. Oct 21)
   num_den_df$period_end <- format(num_den_df$period_end, format = "%b %y")
+  num_den_df$value <- round(as.numeric(num_den_df$value),1)
   # select the columns we need and transpose them
   num_den_table <- dplyr::select(num_den_df, "Date" = period_end, "Numerator" = numerator,
                                   "Denominator" = denominator, "Value" = value) %>%
@@ -17,6 +17,9 @@ num_den_table <- function(input_df){
     t() %>%
     #turns colnames into variables
     as_tibble(rownames = "x", .name_repair = "minimal")
+  num_sum <- sum(as.numeric(num_den_table[2,2:13]))
+  den_sum <- sum(as.numeric(num_den_table[3,2:13]))
+  num_den_table$Total <- c("Total", num_sum, den_sum, round((num_sum/den_sum)*input_df$multiplier[1], digits = 1))
   # Uses the table we just created, passes it to the create table function
   # and adds a custom theme we created for HQIU (see theme function for settings)
   num_den_table %>%
